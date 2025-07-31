8cad13dfb8ac20aabde3f3bda625fc38d3d70cd5
fallssyj <falls_syj@qq.com>
2025-07-31 09:45:48 +0800

    更新

--- generate_config.py
+++ generate_config.py
@@ -13,21 +13,17 @@ file_list = [f for f in files if os.path.isfile(os.path.join(providers_dir, f))]
 output_filename = 'clash.yaml'
 uri = 'https://cdn.jsdelivr.net/gh/fallssyj/Clash/Providers/'
 
+# 读取已有的配置内容
+with open('config.yaml', 'r', encoding='utf-8') as f_in:
+    existing_content = f_in.read()
+
+# 写入到新的配置文件
 with open(output_filename, 'w', encoding='utf-8') as f:
-    # 写入rules部分
-    f.write('rules:\n')
-    for filename in file_list:
-        name_without_extension = os.path.splitext(filename)[0]
-        f.write(f'  - RULE-SET,{name_without_extension} / Domain, \n')
-    
-    f.write('\n')  # 空行分隔不同部分
-    f.write('rule-anchor:\n')
-    f.write('  ip: &ip {type: http, interval: 86400, behavior: ipcidr, format: mrs}\n')
-    f.write('  domain: &domain {type: http, interval: 86400, behavior: domain, format: mrs}\n')
-    f.write('  class: &class {type: http, interval: 86400, behavior: classical, format: text}\n')
-    f.write('\n')  # 空行分隔不同部分
+
+    # 先写入原有配置
+    f.write(existing_content)
+
     # 写入rule-providers部分
-    f.write('rule-providers:\n')
     for filename in file_list:
         name_without_extension = os.path.splitext(filename)[0]
         f.write(f'  {name_without_extension} / Domain: {{<<: *class, url: \"{uri}{filename}\"}}\n')
